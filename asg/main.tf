55d70a27d2236c5d46a9f62c443c2c9ef478bd22
mariadubita <153767089+mariadubita@users.noreply.github.com>
2024-03-16 16:44:21 -0400

    commit

--- asg/main.tf
+++ asg/main.tf
@@ -1,16 +1,19 @@
 module "asg" {
-  source = "terraform-aws-modules/autoscaling/aws"
-  name = "wordpress"
-  # Launch configuration
-  lc_name = "example-lc"
-  image_id        = data.aws_ami.ubuntu.id
-  instance_type   = "t2.micro"
-  # Auto scaling group
-  asg_name                  = "example-asg"
-  vpc_zone_identifier       = data.terraform_remote_state.vpc.outputs.private_subnets
-  health_check_type         = "EC2"
+  source  = "terraform-aws-modules/autoscaling/aws"
+  # Autoscaling group
+  name = "example-asg"
   min_size                  = 0
   max_size                  = 1
   desired_capacity          = 1
   wait_for_capacity_timeout = 0
+  health_check_type         = "EC2"
+  vpc_zone_identifier       = data.terraform_remote_state.vpc.outputs.private_subnets
+  # Launch template
+  launch_template_name        = "example-asg"
+  launch_template_description = "Launch template example"
+  update_default_version      = true
+  image_id        = data.aws_ami.ubuntu.id
+  instance_type     = "t3.micro"
+  ebs_optimized     = false
+  enable_monitoring = false
 }
